fb23f6cb2271f4452a53330fd769b7eec5ce963f
Ricco Førgaard <ricco@fiskeben.dk>
2017-06-25 00:44:23 +0200

    Removed PostgreSQL and Docker
    Much has been removed since it isn't necessary to keep it here. It's
    possible that Docker will need to go back in, time will tell.

--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/etc/init.d/postgresql start; su postgres sh -c 'createuser postgres & createdb postgres'; sudo -u postgres psql -c "ALTER ROLE postgres WITH password 'postgres'"; /etc/init.d/postgresql restart;
